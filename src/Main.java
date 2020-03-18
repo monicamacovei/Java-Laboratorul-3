75f9723f57da8c8381268f639de7448024904335
monicuta2008 <monicuta2008@yahoo.com>
2020-03-18 15:32:29 +0200

    Show results without an algorithm

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Knapsack knapsack = new Knapsack(10);
+        Knapsack knapsackDynamic = new Knapsack(8);
         List<Item> items = new ArrayList<>();
         Item book1 = new Book("Dragon Rising", 3, 5);
         items.add(book1);
@@ -15,11 +16,12 @@ public class Main {
         items.add(food2);
         Item weapon = new Weapon("Sword",5,10);
         items.add(weapon);
-
+        knapsack.setItems(book1, food1, weapon);
+        System.out.println(knapsack.getItems());
 
         Algorithm firstAlgorithm = new Greedy(knapsack,items);
         System.out.println("Final value with greedy algorithm: " + firstAlgorithm.getValue());
-        Algorithm secondAlgorithm = new Dynamic(knapsack,items);
+        Algorithm secondAlgorithm = new Dynamic(knapsackDynamic,items);
         System.out.println("Final value with dynamic programming: " + secondAlgorithm.getValue());
     }
 }
